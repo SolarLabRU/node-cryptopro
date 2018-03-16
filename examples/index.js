4d549293fbb06e2024a2669465fa8545b80e8905
Victor <v.stroganov@solarl.ru>
2018-03-16 14:31:25 +0300

    tests added

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const libCrypto = require('./libCrypto');
+const nodeCryptopro = require('node-cryptopro');
 
 const textToEncode = "text message to encode";
 
@@ -18,7 +18,7 @@ const buffer = Buffer.from(textToEncode);
 const bytesToEncrypt = new Uint8Array(buffer);
 console.log("====Bytes to encode:" + bytesToEncrypt);
 
-let encryptionResult = libCrypto.encrypt(bytesToEncrypt, senderContainerName, responderCertFilename);
+let encryptionResult = nodeCryptopro.encrypt(bytesToEncrypt, senderContainerName, responderCertFilename);
 
 console.log("====encryptedBytesArray:" + encryptionResult.encryptedBytesArray);
 
@@ -30,7 +30,7 @@ let encryptedBytesArray = new Uint8Array([46,44,243,89,248,138,192,196,23,58,255
 let sessionKeyBlob = new Uint8Array([1,32,0,0,30,102,0,0,253,81,74,55,30,102,0,0,246,85,60,206,244,32,49,43,157,220,127,136,22,171,8,132,151,212,123,237,61,137,118,167,205,155,133,200,174,205,7,221,81,235,78,128,22,150,148,73,163,145,76,233,48,11,6,9,42,133,3,7,1,2,5,1,1]);
 let IV = new Uint8Array([102,100,245,44,255,196,107,181]);
 
-let decryptedBytes = libCrypto.decrypt(
+let decryptedBytes = nodeCryptopro.decrypt(
 	encryptionResult.encryptedBytesArray, 
 	responderContainerName,
 	senderCertFilename,
@@ -60,7 +60,7 @@ let Mac = new Uint8Array([12,24,18,44]);
 
 let Ukm = new Uint8Array([140,29,137,127,121,245,125,179]);
 //----------------------------------------
-let decryptedBytes = libCrypto.decrypt(
+let decryptedBytes = nodeCryptopro.decrypt(
 	encryptedBytesArray, 
 	responderContainerName,
 	senderCertFilename,
@@ -76,11 +76,11 @@ console.log("Decrypted message:" + decryptedMessage);
 console.log("Bytes to sign:" + bytesArrayToSign);
 
 
-const signature = libCrypto.signHash(senderContainerName, bytesArrayToSign);
+const signature = nodeCryptopro.signHash(senderContainerName, bytesArrayToSign);
 console.log("Signature:" + signature);
 
 //const signature = new Uint8Array([24,61,148,203,118,4,210,69,139,98,79,169,22,58,15,162,12,172,117,164,132,5,229,153,197,192,39,158,202,2,3,23,141,144,173,28,229,103,48,203,155,160,70,231,234,219,14,184,193,173,247,136,238,202,175,48,181,75,182,117,79,180,191,14]);
-/*const isVerified = libCrypto.verifySignature(bytesArrayToSign, signature, senderCertFilename);
+/*const isVerified = nodeCryptopro.verifySignature(bytesArrayToSign, signature, senderCertFilename);
 if(isVerified) {
 	console.log("Verified");
 } else {
@@ -88,5 +88,5 @@ if(isVerified) {
 }*/
 
 //CreateHash example:
- //const hash = libCrypto.createHash(bytesArrayToSign);
+ //const hash = nodeCryptopro.createHash(bytesArrayToSign);
  //console.log("Hash:" + hash);
--- test/index.js
+++ test/index.js
@@ -2,13 +2,63 @@
 
 const expect = require('chai').expect;
 
+const nodeCryptopro = require('../index');
+
+const senderContainerName = "5973e5bc6-1e43-6206-c603-21fdd08867e";
+const senderCertFilename = "2012_Cert.cer";
+
+const responderContainerName = "5973e5bc6-1e43-6206-c603-21fdd08867e";
+const responderCertFilename =  "2012_Cert.cer";
 
 describe('Тесты', function () {
 
-    const msg = 'test';
+	const sourceMessage = "text message for tests";
+	const buffer = Buffer.from(sourceMessage);
 
-    it('Пустой тест', async () => {
-        expect(msg).to.equal('test');
-    });
+	const sourceMessageBytes = new Uint8Array(buffer);
+	const hashForSourceMessage = new Uint8Array([82,181,47,23,1,228,41,72,41,214,88,194,195,191,190,222,223,73,66,111,196,65,133,235,206,122,89,171,160,130,48,90]);
+
+	let hashSignatureForSourceMessage = "";
+
+	let encryptionResult = {};
+
+	it('Вычисление хеша', async () => {
+		const hash = nodeCryptopro.createHash(sourceMessageBytes);
+
+		expect(hash).to.deep.equal(hashForSourceMessage);
+	});
+
+	it('Вычисление цифровой подписи хеша', async () => {
+		hashSignatureForSourceMessage = nodeCryptopro.signHash(senderContainerName, sourceMessageBytes);
+
+		expect(hashSignatureForSourceMessage).to.have.lengthOf(64);
+	});
+
+	it('Проверка цифровой подписи хеша', async () => {
+		const isVerified = nodeCryptopro.verifySignature(sourceMessageBytes, hashSignatureForSourceMessage, senderCertFilename);
+
+		expect(isVerified).to.equal(true);
+	});
+
+	it('Шифрование сообщения по алгоритму ГОСТ 28147', async () => {
+		encryptionResult = nodeCryptopro.encrypt(sourceMessageBytes, senderContainerName, responderCertFilename);
+
+		expect(encryptionResult.encryptedBytesArray).to.have.lengthOf(sourceMessageBytes.length);
+		expect(encryptionResult.sessionKeyBlob).to.have.lengthOf(73);
+		expect(encryptionResult.IV).to.have.lengthOf(8);
+	});
+
+	it('Дешифрование сообщения по алгоритму ГОСТ 28147', async () => {
+		let decryptedBytes = nodeCryptopro.decrypt(
+			encryptionResult.encryptedBytesArray, 
+			responderContainerName,
+			senderCertFilename,
+			encryptionResult.IV,
+			encryptionResult.sessionKeyBlob);
+
+		const decryptedMessage = (new Buffer(decryptedBytes)).toString();
+
+		expect(decryptedMessage).to.equal(sourceMessage);
+	});
 
 });
